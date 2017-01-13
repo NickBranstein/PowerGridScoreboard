093565cafe569ac4cb3d8b992892086581dea2b8
NickBranstein <nick@brosteins.com>
2017-01-13 14:49:45 -0500

    Initial Commit

---
+++ platforms/ios/build/PowerGridScoreboard.build/Debug-iphonesimulator/PowerGridScoreboard.build/Objects-normal/i386/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/Nick/Git/Repos/PowerGridScoreboard/platforms/ios/internal/main.m \
+  /Users/Nick/Git/Repos/PowerGridScoreboard/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/Nick/Git/Repos/PowerGridScoreboard/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/Nick/Git/Repos/PowerGridScoreboard/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/Nick/Git/Repos/PowerGridScoreboard/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/Nick/Git/Repos/PowerGridScoreboard/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/Nick/Git/Repos/PowerGridScoreboard/platforms/ios/internal/TNSDebugging.h
