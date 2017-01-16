e2999109d8fc50f43726bd7a207dbf8e14032b21
Nick <nickisnick@gmail.com>
2017-01-16 17:16:30 -0500

    Delete playerModel.js

--- app/models/playerModel.js
+++
@@ -1,6 +0,0 @@
-var Player = (function () {
-    function Player() {
-    }
-    return Player;
-}());
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGxheWVyTW9kZWwuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJwbGF5ZXJNb2RlbC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtJQUFBO0lBRUEsQ0FBQztJQUFELGFBQUM7QUFBRCxDQUFDLEFBRkQsSUFFQyIsInNvdXJjZXNDb250ZW50IjpbImNsYXNzIFBsYXllciB7XG4gICAgTmFtZTogc3RyaW5nO1xufSJdfQ==
