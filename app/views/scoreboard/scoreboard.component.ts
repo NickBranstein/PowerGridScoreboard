e6fc262e2fab27da2892877e9baa7d404d89d7b2
NickBranstein <nick@brosteins.com>
2017-01-20 14:13:54 -0500

    started data binding from parent to child

--- app/views/scoreboard/scoreboard.component.ts
+++ app/views/scoreboard/scoreboard.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 import { PageRoute } from "nativescript-angular/router";
 import { ActivatedRoute } from "@angular/router";
-import "rxjs/add/operator/switchMap";
+import { Player } from "../../models/player";
 
 @Component({
     selector: "scoreboard",
@@ -10,11 +10,18 @@ import "rxjs/add/operator/switchMap";
     
 export class ScoreboardComponent {
     public players: string[];
+    public playerOne: Player;
+    public playerTwo: Player;
+    public playerThree: Player;
 
     public constructor(private route: ActivatedRoute) {
         this.route.queryParams.subscribe(params => {
             this.players = params["players"];
             console.log(this.players);
         });
+
+        this.playerOne = new Player("Nick", 100);
+        this.playerTwo = new Player("Jason", 500);
+        this.playerThree = new Player("David", 3400);
     }    
 }
