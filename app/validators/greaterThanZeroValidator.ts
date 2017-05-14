08a67472e0f21347fa6c4ca385eac078f42947f6
NickBranstein <nick@brosteins.com>
2017-05-13 20:12:40 -0400

    updating example

--- app/validators/greaterThanZeroValidator.ts
+++ app/validators/greaterThanZeroValidator.ts
@@ -1,9 +1,10 @@
 import { Validator, AbstractControl, ValidatorFn, NG_VALIDATORS, Validators } from "@angular/forms";
-import { Directive } from "@angular/core";
+import { Directive, Input, OnChanges, SimpleChanges } from "@angular/core";
 
-export function greaterThanZeroValidator() : ValidatorFn {
+export function greaterThanZeroValidator(test: any) : ValidatorFn {
     return (control: AbstractControl): { [key: string]: any; } => {
         let num = parseInt(control.value);
+        //console.log(test);
         
         return control.value === null
             || control.value === undefined
@@ -17,11 +18,24 @@ export function greaterThanZeroValidator() : ValidatorFn {
     providers: [{ provide: NG_VALIDATORS, useExisting: GreaterThanZeroValidatorDirective, multi: true }]
 })
 export class GreaterThanZeroValidatorDirective implements Validator {
+    @Input() greaterThanZero: any;
     private valFn = Validators.nullValidator;
 
     constructor() {
-        this.valFn = greaterThanZeroValidator();
+        this.valFn = greaterThanZeroValidator(this.greaterThanZero);
+    }
 
+    ngOnChanges(changes: SimpleChanges): void {
+        const change = changes['greaterThanZero'];
+        if (change) {
+            let otherValue = change.currentValue;
+            console.log(otherValue);
+            //const val: string | RegExp = change.currentValue;
+            //const re = val instanceof RegExp ? val : new RegExp(val, 'i');
+            this.valFn = greaterThanZeroValidator(this.greaterThanZero);
+        } else {
+            this.valFn = Validators.nullValidator;
+        }
     }
 
     validate(control: AbstractControl): { [key: string]: any } {
