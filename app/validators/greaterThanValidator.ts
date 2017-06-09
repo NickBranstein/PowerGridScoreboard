b0c22075aff69476ae71704d3ea0d5f7e508757f
NickBranstein <nick@brosteins.com>
2017-06-09 09:33:21 -0400

    added gt 0 validation

--- app/validators/greaterThanValidator.ts
+++ app/validators/greaterThanValidator.ts
@@ -9,13 +9,13 @@ export class GreaterThanValidatorDirective implements Validator {
     @Input() greaterThan: any;
 
     validate(control: AbstractControl): { [key: string]: any } {
-        console.log(this.greaterThan);
-        console.log(control.value);
         let num = parseInt(control.value);
+        let greaterThan = parseInt(this.greaterThan);
 
         return control.value === null
             || control.value === undefined
             || control.value === ''
-            || num > this.greaterThan ? null : { 'greaterThan': { valid: false } };
+            || isNaN(greaterThan)
+            || num > greaterThan ? null : { 'greaterThan': { valid: false } };
     }
 }
